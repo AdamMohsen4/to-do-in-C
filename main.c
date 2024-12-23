ad0f306bb8d32462eed63cd9d04c260b780f7d4d
AdamMohsen4 <adam.amohsen4@gmail.com>
2024-12-23 01:32:47 +0100

    Refactor main.c to improve task management functionality and remove launch.json

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 // Import libraries
 #include <stdio.h> // Standard Input/Output library
 #include <stdlib.h> // Standard library
+#include <string.h> // Include string.h for strcspn
 
 /**
  * This program is a to-do list that allows the user to add, list, mark as done, edit, and delete tasks.
  * It appends the tasks to a file calles "tasks.txt".
+ * 
+ * @author: Adam Abulmajid
  */
 #define MAX_TASKS 100 // Maximum number of tasks
 
 typedef struct Task {
-    int id;
-    char description[256];
-    int done; // 0 for not done, 1 for done
-} Task;
+    int id; // Task ID
+    int done; // 1 if the task is done, 0 otherwise
+    char description[256]; // Task description
+}Task;
 
 Task tasks[MAX_TASKS]; // Array of tasks
 int task_count = 0; // Number of tasks
 
-// Function prototypes
+// Function for the to-do list
 void add_task();
 void list_tasks();
 void mark_as_done();
@@ -26,67 +29,22 @@ void delete_task();
 void save_tasks();
 void load_tasks();
 
-int main(){
-    loadTasks();
-    int choice;
 
-
-do {
-    printf("\n===== To-Do List =====\n");
-    printf("1. Add Task\n");
-    printf("2. List Tasks\n");
-    printf("3. Mark as Done\n");
-    printf("4. Edit Task\n");
-    printf("5. Delete Task\n");
-    printf("6. Save and Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-    getchar(); // Consume the newline character
-
-    switch (choice) {
-        case 1:
-            add_task();
-            break;
-        case 2:
-            list_tasks();
-            break;
-        case 3:
-            mark_as_done();
-            break;
-        case 4:
-            edit_task();
-            break;
-        case 5:
-            delete_task();
-            break;
-        case 6:
-            save_tasks();
-            break;
-        default:
-            printf("Invalid choice. Please try again.\n");
-    
-}
-}while (choice != 6);
-
-    return 0;
-}
-
-
-// Add a new task
+// Add a new task 
 void add_task() {
     if (task_count == MAX_TASKS) {
         printf("You have reached the maximum number of tasks.\n");
         return;
     }
 
-    Task task;
-    task.id = task_count + 1;
+    Task task; // Create a new task
+    task.id = task_count + 1; // Increment the task ID
     printf("Enter task description: ");
-    fgets(task.description, 256, stdin);
+    fgets(task.description, 256, stdin); // Read task description
     task.description[strcspn(task.description, "\n")] = 0; // Remove the newline character
     task.done = 0; // Not done
 
-    tasks[task_count++] = task;
+    tasks[task_count++] = task; // Add the task to the array
 }
 
 // List all tasks
@@ -107,7 +65,7 @@ void list_tasks() {
 void mark_as_done() {
     int id;
     printf("Enter the task ID: ");
-    scanf("%d", &id);
+    scanf("%d", &id); // Read task ID
 
     for (int i = 0; i < task_count; i++) {
         if (tasks[i].id == id) {
@@ -130,7 +88,7 @@ void edit_task() {
         if (tasks[i].id == id) {
             printf("Enter new task description: ");
             getchar(); // Consume the newline character
-            fgets(tasks[i].description, 256, stdin);
+            fgets(tasks[i].description, 256, stdin); // Read task description
             tasks[i].description[strcspn(tasks[i].description, "\n")] = 0; // Remove the newline character
             printf("Task updated.\n");
             return;
@@ -146,6 +104,7 @@ void delete_task() {
     printf("Enter the task ID: ");
     scanf("%d", &id);
 
+//Instead of really deleting the task, I will move the tasks to the left
     for (int i = 0; i < task_count; i++) {
         if (tasks[i].id == id) {
             for (int j = i; j < task_count - 1; j++) {
@@ -162,7 +121,7 @@ void delete_task() {
 
 // Save tasks to a file
 void save_tasks() {
-    FILE *file = fopen("tasks.txt", "w");
+    FILE *file = fopen("tasks.txt", "w"); // Open file in write mode
     if (file == NULL) {
         printf("Error opening file.\n");
         exit(1);
@@ -183,7 +142,7 @@ void load_tasks() {
     if (file == NULL) {
         return;
     }
-
+    // Read tasks from the file by reading the ID, done status, and description
     while (fscanf(file, "%d;%d;", &tasks[task_count].id, &tasks[task_count].done) == 2) {
         fgets(tasks[task_count].description, 256, file);
         tasks[task_count].description[strcspn(tasks[task_count].description, "\n")] = 0; // Remove the newline character
@@ -192,4 +151,47 @@ void load_tasks() {
 
     fclose(file);
     printf("Tasks loaded from file.\n");
+}
+
+int main(){
+    load_tasks(); // Load tasks from file
+    int choice;
+
+    do {
+        printf("\n===== To-Do List =====\n");
+        printf("1. Add Task\n");
+        printf("2. List Tasks\n");
+        printf("3. Mark as Done\n");
+        printf("4. Edit Task\n");
+        printf("5. Delete Task\n");
+        printf("6. Save and Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice); 
+        getchar(); // Consume the newline character (meaning the enter key)
+
+        switch (choice) {
+            case 1:
+                add_task();
+                break;
+            case 2:
+                list_tasks();
+                break;
+            case 3:
+                mark_as_done();
+                break;
+            case 4:
+                edit_task();
+                break;
+            case 5:
+                delete_task();
+                break;
+            case 6:
+                save_tasks();
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 6);
+
+    return 0;
 }
